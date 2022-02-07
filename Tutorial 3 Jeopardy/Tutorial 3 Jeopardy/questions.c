36f460178beda76b43ef0d440fd45732ca4f3656
Joshua <74574379+JoshuaWhite0110@users.noreply.github.com>
2022-02-07 11:03:58 -0500

    Update questions.c

--- Tutorial 3 Jeopardy/Tutorial 3 Jeopardy/questions.c
+++ Tutorial 3 Jeopardy/Tutorial 3 Jeopardy/questions.c
@@ -13,7 +13,60 @@
  // Initializes the array of questions for the game
 void initialize_game(void)
 {
-    // initialize each question struct and assign it to the questions array
+
+    strcpy(questions[0].category, "Meeme's");
+    strcpy(questions[0].question, "In this move the character 'Kronk' makes his debute");
+    strcpy(questions[0].answer, "Emporers new Groove");
+    questions[0].value = 100;
+    questions[0].answered =  false;
+
+    strcpy(questions[1].category, "Meeme's");
+    strcpy(questions[1].question, "It May not be much but it is 'blank' work");
+    strcpy(questions[1].answer, "Honest");
+    questions[1].value = 200;
+    questions[1].answered =  false;
+
+    strcpy(questions[2].category, "Meeme's");
+    strcpy(questions[2].question, "This famous rapper has been imortalized in a classic no, yes template");
+    strcpy(questions[2].answer, "Drake");
+    questions[2].value = 400;
+    questions[2].answered =  false;
+
+    strcpy(questions[3].category, "Meeme's");
+    strcpy(questions[3].question, "This group of 4 spider man valains are consindered to be the 'blank'");
+    strcpy(questions[3].answer, "Boys");
+    questions[3].value = 800;
+    questions[3].answered =  false;
+
+    strcpy(questions[4].category, "Moke");
+    strcpy(questions[4].question, "This lage orange moke's is commonly known as an");
+    strcpy(questions[4].answer, "Oranotang");
+    questions[4].value = 100;
+    questions[4].answered =  false;
+
+    strcpy(questions[5].category, "Moke");
+    strcpy(questions[5].question, "In the movie planet of the apes the chracter Ceser is this spcieces of moke");
+    strcpy(questions[5].answer, "Chimpanze");
+    questions[5].value = 200;
+    questions[5].answered =  false;
+
+    strcpy(questions[6].category, "Moke");
+    strcpy(questions[6].question, "In this meme template a chipanze is urging readers to return to 'blank'");
+    strcpy(questions[6].answer, "Monke");
+    questions[6].value = 400;
+    questions[6].answered =  false;
+
+    strcpy(questions[7].category, "Moke");
+    strcpy(questions[7].question, "This subspicies of monke is native to only this african island");
+    strcpy(questions[7].answer, "Madagascar");
+    questions[7].value = 800;
+    questions[7].answered =  false;
+
+    strcpy(questions[8].category, "");
+    strcpy(questions[8].question, "");
+    strcpy(questions[8].answer, "");
+    questions[8].value = 200;
+    questions[8].answered =  false;
 }
 
 // Displays each of the remaining categories and question dollar values that have not been answered
